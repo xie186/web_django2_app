80573bda49e30b27b91b78794655f7aa3fa28d57
xie186 <xie186@purdue.edu>
2019-05-02 10:50:13 -0400

    Test for about us page

--- main/tests.py
+++ main/tests.py
@@ -6,4 +6,10 @@ class TestPage(TestCase):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "home.html")
-        self.assertContains(response, "Hello, world")
+        self.assertContains(response, "BookTime")
+
+    def test_about_us_page_works(self):
+        response = self.client.get("/about-us/")
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response, 'about_us.html')
+        self.assertContains(response, 'BookTime')
