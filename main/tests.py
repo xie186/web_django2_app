16e1bbedc47064ec809318a87100d0351a9c95bf
xie186 <xie186@purdue.edu>
2019-05-04 13:48:26 -0400

    Add test for contact us page

--- main/tests.py
+++ main/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse 
+from main import forms
 
 # Create your tests here.
 class TestPage(TestCase):
@@ -14,3 +15,12 @@ class TestPage(TestCase):
         self.assertEqual(response.status_code,200)
         self.assertTemplateUsed(response, 'about_us.html')
         self.assertContains(response, 'BookTime')
+
+    def test_contact_us_page_works(self):
+        response = self.client.get(reverse("contact_us"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact_form.html')
+        self.assertContains(response, 'BookTime')
+        self.assertIsInstance(
+            response.context["form"], forms.ContactForm
+        )
